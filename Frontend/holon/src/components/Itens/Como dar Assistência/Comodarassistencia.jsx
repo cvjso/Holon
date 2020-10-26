af9e4ce6e3873f48e4cc84c527bab530759b820f
julioribeiiro <jcrm@cesar.school>
2020-10-26 08:50:54 -0300

    Inicio front

---
+++ Frontend/holon/src/components/Itens/Como dar Assistência/Comodarassistencia.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Comodarassistencia.css';
+
+export default function Comodarassistencia() {
+  return <h1 className='como-dar-assistencia'>como dar assistencia</h1>;
+}
