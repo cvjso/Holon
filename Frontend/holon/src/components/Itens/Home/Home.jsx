3a49b2f27174ba84917ec1d2096480f8a9d7cd29
julioribeiiro <jcrm@cesar.school>
2020-10-26 19:31:44 -0300

    Tela Inicial

--- Frontend/holon/src/components/Itens/Home/Home.jsx
+++ Frontend/holon/src/components/Itens/Home/Home.jsx
@@ -3,8 +3,13 @@ import './Home.css';
 
 export default function Home() {
   return (
-    <>
-      <h1 className='home'>Pagina inicial</h1>
-    </>
+    <div className="mainDiv">
+      <h1 className='topText' id="text-attributes">A mudança começa com você</h1>
+      <h1 className='bottomText' id="text-attributes">Faça parte de uma rede de auxílio dedicada a ajudar pessoas em situação de rua através de uma simples plataforma online</h1>
+      <div className="btnChamado">
+        <a href="/" className="linkChamado">Abrir um chamado</a>
+      </div>
+      
+    </div>
   );
 }
