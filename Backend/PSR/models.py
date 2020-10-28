44d088ff65e2102b4cda1b5b90769793fc54a02b
tenonin <luante2000@gmail.com>
2020-10-28 11:28:08 -0300

    add new columns:Dia,Nome,Apelido but the column Dia is with wrong time need to fix

--- Backend/PSR/models.py
+++ Backend/PSR/models.py
@@ -31,12 +31,15 @@ ListaRoupas = {
 # Create your models here.
 class CadastroPSR(models.Model):
     GRAU = [("LOW", 'Baixa'),("MED", 'Media'),("HIGH",'Alta')]
+    Nome = models.CharField(max_length=120,blank=True,default='')
+    Apelido = models.CharField(max_length=120,blank=True,default='')
     Endereco = models.CharField(max_length=120,blank=True)
     Bairro = models.CharField(max_length=120,blank=True)
     Cidade = models.CharField(max_length=120,blank=True)
     Referencia = models.CharField(max_length=120,blank=True)
     Descricao = models.TextField(max_length=120, blank=True)
     GrauPrioridade = models.CharField(max_length=120, choices=GRAU)
+    Dia = models.DateTimeField(auto_now=True,)
 
 class NecessidadesPSR(models.Model):
     Psr = models.ForeignKey(CadastroPSR, on_delete=models.CASCADE)
