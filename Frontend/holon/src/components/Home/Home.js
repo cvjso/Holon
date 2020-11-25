b60966a1e7570f19575ef7fce0f983d305c1fdb1
julioribeiiro <jcrm@cesar.school>
2020-11-25 02:41:57 -0300

    Tela chamado registrado

--- Frontend/holon/src/components/Home/Home.js
+++ Frontend/holon/src/components/Home/Home.js
@@ -4,12 +4,43 @@ import './Home.css';
 import Cookies from 'universal-cookie';
 import Facebook from '../FacebookBtn/index';
 import GoogleBtn from '../GoogleBtn/index';
+import { render } from '@testing-library/react';
 
 const cookies = new Cookies();
+
+class ListaChamados extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {chamados: this.props.chamados}
+    }
+
+        render() {
+            return(
+                <div>
+                    {
+                        this.state.chamados.map(c => {
+                            return (
+                                <div className='ReportBox'>
+                                    <h3 className="TextoReportBox">Chamado Registrado!</h3>
+                                    <h6 className="EditButton">Editar</h6>
+                                    <p>{c.data}</p>
+                                    <div className="Details">
+                                        <Link>Detalhes</Link>
+                                    </div>
+                                </div>
+                            );
+                        })
+                    }
+                </div>
+            );
+        }
+}
 
 export class Home extends Component{
     constructor(props){
         super(props);
+
+        
 
         this.state = {
                 reporte: false,
@@ -35,21 +66,21 @@ export class Home extends Component{
     }
     
     render() {
-        if (this.state.reporte === true) {
+        //if (this.state.reporte === false) {
+        if (true) {
             return (
-                <>
-                <div className='ReportBox'>
-                <h3 className="TextoReportBox">Chamado Registrado!</h3>
-                <h6 className="EditButton">Editar</h6>
-                <p>{this.state.curTime}</p>
-                <Link className="Details">Detalhes</Link>
+                <div>
+                    <ListaChamados chamados={[
+                        {data: "09/10/2020, 10:30"},
+                        {data: "01/11/2020, 15:22"},
+                    ]} />
+
+                    <div className='Text' id="text-attributes">A mudança começa com você</div>
+                    <div className='bottomText' id="text-attributes">Faça parte de uma rede de auxílio dedicada a ajudar pessoas em situação de rua através de uma simples plataforma online</div>
+                    <div className="btnChamado">
+                    {<a href="/" className="linkChamado">Abrir um chamado</a>}
+                    </div>
                 </div>
-                <div className='Text' id="text-attributes">A mudança começa com você</div>
-                <div className='bottomText' id="text-attributes">Faça parte de uma rede de auxílio dedicada a ajudar pessoas em situação de rua através de uma simples plataforma online</div>
-                <div className="btnChamado">
-                {<a href="/" className="linkChamado">Abrir um chamado</a>}
-                </div>
-                </>
             );
         }
         else {
